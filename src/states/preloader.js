ae8d643dc3e7a1c0deef9d2debb1b1a18858db2c
Benjamin Bruns <benjamin.bruns@namics.com>
2016-10-13 17:14:04 +0200

    change background

--- src/states/preloader.js
+++ src/states/preloader.js
@@ -25,7 +25,14 @@ class Preloader extends Phaser.State {
     loadResources() {
         // load your resources here
         this.game.load.spritesheet('bricksspritesheet', 'assets/bricks.png', 64, 32, 20, 1);
-        this.game.load.image('starfield', 'assets/misc/starfield.jpg');
+        this.game.load.image('castle', 'assets/backgrounds/uncolored_castle.png');
+        this.game.load.image('desert', 'assets/backgrounds/uncolored_desert.png');
+        this.game.load.image('forest', 'assets/backgrounds/uncolored_forest.png');
+        this.game.load.image('hills', 'assets/backgrounds/uncolored_hills.png');
+        this.game.load.image('peaks', 'assets/backgrounds/uncolored_peaks.png');
+        this.game.load.image('piramids', 'assets/backgrounds/uncolored_piramids.png');
+        this.game.load.image('plain', 'assets/backgrounds/uncolored_plain.png');
+        this.game.load.image('talltrees', 'assets/backgrounds/uncolored_talltrees.png');
         this.game.load.image('paddle', 'assets/paddle.png');
     }
 
--- src/util/utils.js
+++ src/util/utils.js
@@ -4,7 +4,7 @@ class Utils {
     }
 
     static loadBackground(game) {
-        game.add.tileSprite(game.width * 0.5 - 400, game.height * 0.5 - 300, 800, 600, 'starfield');
+        game.add.tileSprite(game.width * 0.5 - 400, game.height * 0.5 - 524, 1024, 1024, 'castle');
     }
 
 }
