4259b33adbc084a675f95ad76d5a7192a2d11f5b
Benjamin Justice <Benjamin.Justice@Namics.com>
2016-10-14 09:30:40 +0200

    renamed Game to Level to reduce confusion with Phasers this.game

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
- import Boot from './states/boot';
- import Game from './states/game';
- import Preloader from './states/preloader';
- import Gameover from './states/gameover';
+import Boot from './states/boot';
+import Level from './states/level';
+import Preloader from './states/preloader';
+import Gameover from './states/gameover';
 
 
 const game = new Phaser.Game(1024, 576, Phaser.AUTO, 'NamicsJam16-game');
 
- game.state.add('boot', new Boot());
- game.state.add('game', new Game());
- game.state.add('preloader', new Preloader());
- game.state.add('gameover', new Gameover());
+game.state.add('boot', new Boot());
+game.state.add('level', new Level());
+game.state.add('preloader', new Preloader());
+game.state.add('gameover', new Gameover());
 
 game.state.start('boot');
--- src/states/level.js
+++ src/states/level.js
@@ -57,4 +57,4 @@ class Level extends Phaser.State {
     }
 }
 
-export default Game;
+export default Level;
