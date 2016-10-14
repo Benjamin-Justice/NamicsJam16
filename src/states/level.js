d87b3c6b7efe05dc2a1324131790fd390bccf5e8
Benjamin Justice <Benjamin.Justice@Namics.com>
2016-10-14 09:25:58 +0200

    refactored game.js to level.js; new resolution 1024 x 576 (16:9)

--- src/states/level.js
+++ src/states/level.js
@@ -4,7 +4,7 @@ import Utils from '../util/utils';
 import Paddle from '../prefabs/paddle';
 import Ball from '../prefabs/ball';
 
-class Game extends Phaser.State {
+class Level extends Phaser.State {
 
     constructor() {
         super();
@@ -18,12 +18,7 @@ class Game extends Phaser.State {
     }
 
     create() {
-        var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, 'Game', {
-            font: '42px Arial', fill: '#ffffff', align: 'center'
-        });
-        text.anchor.set(0.5);
-
-        this.input.onDown.add(this.endGame, this);
+        // this.input.onDown.add(this.endGame, this);
 
         this.addPaddle();
         this.addBall();
